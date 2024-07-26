c609eac6c78914c14792060134e195ad7de12296
fert-dujmayoon <fert.dujmayoon@nfq.com>
2024-07-26 16:31:02 +0700

    refactor: refactor table component

--- src/components/activeIssuesTable/index.tsx
+++ src/components/activeIssuesTable/index.tsx
@@ -13,7 +13,7 @@ import {
 import { useActiveIssuesStore } from "@/store/ActiveIssuesStore";
 import { StatusChip } from "../statusChip";
 import { useTranslation } from "react-i18next";
-import StatusDropdown from "../statusChip";
+import { CustomTable } from "../customTable";
 
 export const ActiveIssuesTable: React.FC = () => {
   const formatTimestamp = (timestamp: string | undefined) => {
@@ -27,75 +27,5 @@ export const ActiveIssuesTable: React.FC = () => {
     fetchActiveIssues();
   }, [activeIssues.length, activeIssues]);
 
-  return (
-    <Box className="overflow-scroll max-w-full pb-[15px]">
-      <TableContainer
-        component={Paper}
-        elevation={0}
-        sx={{
-          borderRadius: 2,
-          overflow: "hidden",
-          border: "1px solid",
-          borderColor: "divider",
-        }}
-      >
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell className="w-[150px]">{t("machine")}</TableCell>
-              <TableCell className="w-[300px]">{t("issue_detail")}</TableCell>
-              <TableCell className="w-[300px]">{t("fix_detail")}</TableCell>
-              <TableCell>{t("assignee")}</TableCell>
-              <TableCell className="w-[150px] text-center">
-                {t("status")}
-              </TableCell>
-              <TableCell className="w-[120px]">{t("updated_at")}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {activeIssues.map((Issue) => (
-              <TableRow key={Issue.issue_id}>
-                <TableCell>
-                  <Typography variant="body2" color="textPrimary">
-                    {Issue.machine_part.name || "N/A"}
-                  </Typography>
-                  <Typography variant="caption" color="textSecondary">
-                    #{Issue.machine_id || "N/A"}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="body2" color="textSecondary">
-                    {Issue.issue_detail || "-"}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="body2" color="textSecondary">
-                    {Issue.tech_detail || "-"}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="body2" color="textSecondary">
-                    {Issue.technician_id || t("unassigned")}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <StatusChip
-                    issueId={Issue.issue_id}
-                    status={Issue.status}
-                    techDetail={Issue.tech_detail || ""}
-                    clickable
-                  />
-                </TableCell>
-                <TableCell>
-                  <Typography variant="body2" color="textSecondary">
-                    {formatTimestamp(Issue.updated_at)}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
-  );
+  return <CustomTable data={activeIssues} statusClickable assigneeClickable />;
 };
